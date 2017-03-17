2a8cbbbc896214b450a443ed8ea5bd4d3ad7d797
Tristan Rubadeau <tristan@rubadeau.com>
2017-03-17 10:37:17 -0600

    clearance views and dashboards controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,16 @@
 Rails.application.routes.draw do
+
+  resources :dashboards, only: :show
+
+  constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
+    root to: "admin/dashboards#show", as: :admin_root
+  end
+
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: "dashboards#show", as: :signed_in_root
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: "marketing#index"
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+require "clearance/rspec"
 ENV["RACK_ENV"] = "test"
 
 require File.expand_path("../../config/environment", __FILE__)
